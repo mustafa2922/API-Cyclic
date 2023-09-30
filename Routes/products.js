22dc93f861457e8cf0cd7dee8ed650718fe13fc2
Mustafa <mustafaraza6372@gmail.com>
2023-09-30 17:34:18 +0500

    single comment

--- Routes/products.js
+++ Routes/products.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router()
 import { PRODUCTS } from '../constants.js'
-
+// controler
 router.get('/', (req, res) => {
     res.send({Products: PRODUCTS })
 })
